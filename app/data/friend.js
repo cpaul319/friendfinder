cf69977ed368b093cb51480e4a682de5f126b4b3
Chris Paul <cpaul319@users.noreply.github.com>
2019-04-23 14:15:59 -0400

    update

---
+++ app/data/friend.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+var spooksArr = [
+
+    {
+        name: "Nicole",
+        photo: "https://i.pinimg.com/originals/ce/57/33/ce5733d2ea952ab7b9c1a27266905cd0.png",
+        scores: [5, 5, 3, 3, 3, 3, 3, 4, 5, 2]
+    },
+
+    {
+        name: "Lance",
+        photo: "https://i.imgflip.com/15z8z7.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Facundo",
+        photo: "https://timedotcom.files.wordpress.com/2016/04/jon-snow-game-of-thrones.jpeg",
+        scores: [2, 3, 2, 3, 2, 3, 5, 4, 4, 3]
+    },
+    {
+        name: "Igor",
+        photo: "https://cdn.mos.cms.futurecdn.net/jkAZ5cu36EwVpigeqG8Gnb.jpg",
+        scores: [2, 3, 3, 5, 5, 3, 4, 4, 2, 2]
+    },
+    {
+        name: "Tori",
+        photo: "https://img.purch.com/h/1000/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA1Mi8zNjUvb3JpZ2luYWwvcmVkLXdoaXRlLXdpbmUtMTIwMTExLmpwZw==https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/margarey-tyrell-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+        scores: [2, 3, 4, 2, 2, 3, 3, 3, 4, 5]
+    },
+    {
+        name: "Greg",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/peter-dinklage-look-1536330273.jpg?resize=480:*",
+        scores: [1, 2, 2, 3, 3, 2, 2, 3, 3, 3]
+    },
+    {
+        name: "Gabriel",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1400-sean-bean-main-hp-1540205054.jpg?crop=1xw:0.8567931456548348xh;center,top&resize=480:*",
+        scores: [3, 2, 2, 2, 2, 2, 2, 2, 4, 3]
+    },
+    {
+        name: "Bryan",
+        photo: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2016%2F06%2F02032506%2Ftormund.jpg&w=450&c=sc&poi=face&q=85",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Tim",
+        photo: "https://i.pinimg.com/originals/5c/cb/3f/5ccb3f46c40f5a2ab3a76dc9e460efdb.jpg",
+        scores: [2, 1, 2, 2, 3, 3, 5, 5, 3]
+    },
+    {
+        name: "Nitza",
+        photo: "https://d1nao0k9edgivc.cloudfront.net/wp-content/uploads/2013/04/Game-Of-Thrones-Season-3-Arya-Stark.jpg",
+        scores: [1, 1, 2, 2, 2, 3, 4, 4, 4, 2]
+    },
+    {
+        name: "Doug",
+        photo: "https://411mania.com/wp-content/uploads/2015/12/Macauley-Culkin-Home-Alone-Adult-645x370.jpg",
+        scores: [1, 2, 1, 3, 1, 3, 1, 2, 2, 1]
+    },
+    {
+        name: "Chris",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Jackie_Coogan_as_Uncle_Fester_%28The_Addams_Family%2C_1966%29.jpg/250px-Jackie_Coogan_as_Uncle_Fester_%28The_Addams_Family%2C_1966%29.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }
+]
+
+module.exports = spooksArr;
+
+
